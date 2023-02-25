e57ebf8c6d7a6bc41dc8f503377306508cfa5b9e
Victor Barros <victorclovisb@hotmail.com>
2023-02-24 23:11:53 -0300

    Change base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'https://api-food-explorer-hhv1.onrender.com'
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://api-food-explorer-hhv1.onrender.com'
+  // baseURL: 'http://localhost:3333'
 })
